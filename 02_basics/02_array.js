d80a40211c4c557a9238dd8f5b59d726fe976e0f
raman919115 <raman919115@gmail.com>
2025-01-01 20:10:17 +0530

    add few notes

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -21,11 +21,11 @@ console.log(real_another_array);
 
 
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"));
-console.log(Array.from({name:"hitesh"}))    //interesting
+console.log(Array.from("Hitesh"));         //typeof this is object
+console.log(Array.from({name:"hitesh"}))    //interesting   typeof this is object
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+console.log(Array.of(score1,score2,score3));  // convert all datatypes into one single array
