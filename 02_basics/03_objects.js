1a168309cf9378b496e0e8e4e5026e51854e5856
raman919115 <raman919115@gmail.com>
2025-01-02 19:35:13 +0530

    few notes

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,5 @@
 // singleton
-// Object.create
+// Object.create    it is used to define objects as constructor
 
 // objects ko agar literals ki tarah declare krein to singleton nhi bnta hai
 // constructor se agar declare krein to hamesha singleton banega
@@ -11,7 +11,7 @@ const mySym = Symbol("key1")   // declaring symbol datatype
 const JsUser = {
     name : "Hitesh",
     "full name" : "Hitesh Choudhary",
-    [mySym]: "myKey1",
+    [mySym]: "myKey1",      // declaring symbol datatype in objects
     age : 18,
     location : "Jaipur",
     email : "hitesh@google.com",
@@ -20,7 +20,8 @@ const JsUser = {
 }
 
 // console.log(JsUser.email);    // first method to acces objects 
-// console.log(JsUser["email"]); // second method to access objects 
+// console.log(JsUser["email"]); // second method to access objects and access symbol from objects
+                                // this method is used specially where the key name has two or more words
 
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym]);     //access symbol value from objects
