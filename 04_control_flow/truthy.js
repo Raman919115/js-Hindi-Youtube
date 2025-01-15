6af16eabb2b2fcb35aad67c1d4130edd40a20f8c
raman919115 <raman919115@gmail.com>
2025-01-15 20:18:43 +0530

    truthy complete

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,57 @@
+const userEmail = "h@hitesh.ai"  // empty array [] bhi truthy value return krta h
+                                // empty string "" falsy value return krta h 
+// if(userEmail){
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){} empty function
+
+const userEmail2 = []
+
+if(userEmail2.length === 0){         // check array whether it is empty or not
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+// some equality checks
+// false == 0
+// true
+
+// false == ''
+// true
+
+// 0 == ''
+// true
+
+
+//  Nullish Coalescing Operater (??) : null undefined
+//  this operater is mainly around on these two keywords
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+
+
+// console.log(val1);
+
+// Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
