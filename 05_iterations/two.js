6deaf47d28810d968a36ba32ae0dd1b1f886ce85
raman919115 <raman919115@gmail.com>
2025-01-16 21:37:51 +0530

    while & do while done

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// while
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value of index is ${myArray[arr]}`);
+    arr = arr + 1  
+}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let num = 11
+
+do {
+    console.log(`num is ${num}`);
+    num++
+} while (num <= 10);
